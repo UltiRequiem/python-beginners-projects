a3efa91fc6299a0815522e7cc80c0aeed65e3751
ataneja21 <ataneja1@terpmail.umd.edu>
2023-12-15 16:34:13 -0500

    first

---
+++ story-generator/french_text.py
@@ -0,0 +1,24 @@
+# Sentence starter
+PREMIERE_PHRASE = ["Il y a environ 100 ans", "En l'an 20 avant J.C.", "Il était une fois"]
+
+# characters
+PERSONNAGE_PRINCIPAL = [
+    " il y avait un roi.",
+    " il y avait un homme qui s'appelait Jack.",
+    " il y avait un fermier.",
+]
+
+# Time
+TEMPS = [" Un jour", " Une nuit"]
+
+# Plot
+INTRIGUE = [" il regardait les montages quand", " il a fait une pique-nique quand"]
+
+# Second characters
+DEUXIEME_PERSONNAGE = [" il a vu un homme", " il a vu une dame"]
+
+# age
+AGE_FRENCH = [" qu'il avait 20 ans et qui", " qui avait 60 ans et qui"]
+
+# work
+TRAVAIL = [" cherchant quelque chose.", " dormait"]
